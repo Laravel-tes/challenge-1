e11bcd6245809f037d5d18716d10eb536eb71996
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-06-03 17:09:44 +0100

    feat: create a task model

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validated = $request ->validat([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required:min:6',
+        ]);
+
+        $user = \App\Models\User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json(['token' => $token], 201);
+
+    }
 }
